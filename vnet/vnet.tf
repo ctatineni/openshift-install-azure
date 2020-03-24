be050cee57537961cb826bb5081e3ab28f6912d6
ctatineni <chandhubabu.thathineni@ibm.com@chandhubabus-MacBook-Pro.local>
2020-03-23 19:19:56 -0500

    adding route table

--- vnet/vnet.tf
+++ vnet/vnet.tf
@@ -9,6 +9,22 @@ resource "azurerm_route_table" "route_table" {
   name                = "${var.cluster_id}-node-routetable"
   location            = var.region
   resource_group_name = var.resource_group_name
+
+  route {
+    name                   = "${var.cluster_id}-node-route"
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "Internet"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "master_association" {
+  subnet_id      = azurerm_subnet.master_subnet.id
+  route_table_id = azurerm_route_table.route_table.id
+}
+
+resource "azurerm_subnet_route_table_association" "worker_association" {
+  subnet_id      = azurerm_subnet.worker_subnet.id
+  route_table_id = azurerm_route_table.route_table.id
 }
 
 locals {
